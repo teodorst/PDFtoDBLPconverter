15ea97cee380f1bd1ca75e2134a2e49707d4f6c9
Teodor Stefu <teodor.stefu@padawan.devacademy.ro>
2016-02-19 13:54:38 +0200

    got the right regex

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -1,25 +1,141 @@
 package converter;
 
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
+import org.apache.pdfbox.io.RandomAccessRead;
+import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.text.PDFTextStripper;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by teodor on 18.02.2016.
  */
 public class Main {
     public static void main(String[] args ) throws IOException {
-        PDDocument document = new PDDocument();
-        // Create a new blank page and add it to the document
-        PDPage blankPage = new PDPage();
-        document.addPage( blankPage );
 
-        // Save the newly created document
-        document.save("BlankPage.pdf");
+        PDFTextStripper pdfStripper = null;
+        PDDocument pdfDocument = null;
+        COSDocument cosDocument = null;
+        File inputFile = null;
+        PDFParser parser = null;
 
-        // finally make sure that the document is properly
-        // closed.
-        document.close();
+        ArrayList<Publication> publications = new ArrayList<Publication>();
+
+        int jumpIndex = 0;
+
+        try {
+            inputFile = new File("RoCHI-2015-proceedings-vfinal.pdf");
+            parser = new PDFParser(new RandomAccessBufferedFileInputStream(inputFile));
+            parser.parse();
+            cosDocument = parser.getDocument();
+            pdfStripper = new PDFTextStripper();
+            pdfDocument = new PDDocument(cosDocument);
+
+            // aici incepe cuprinsul
+            // daca nu ii dau setEndPage atunci o sa citeasca by default tot pdf-ul
+            pdfStripper.setStartPage(3);
+            pdfStripper.setEndPage(4);
+            String parsedText = pdfStripper.getText(pdfDocument);
+            if( parsedText.contains("TABLE OF CONTENTS") ) {
+                jumpIndex = parsedText.indexOf("TABLE OF CONTENTS");
+                jumpIndex += 18; // 17 lungimea + \n
+                parsedText = parsedText.substring(jumpIndex);
+                jumpIndex = 0;
+                String title = "";
+                String author = "" ;
+                String currentString = "";
+                int pageIndex = 0;
+                //start parsing
+                // i will exit when i will have the author index or smth
+
+                //System.out.println(parsedText);
+
+//
+//                while( parsedText.length() != 0 )  {
+//                    jumpIndex = parsedText.indexOf('\n');
+//                    currentString = parsedText.substring(jumpIndex);
+//                    parsedText = parsedText.substring(jumpIndex + 1);
+//
+//                    // it's empty line
+//
+//
+//                    if( !currentString.contains("\\w+") )
+//                        continue;
+//                    System.out.println(currentString);
+//
+//                    if( currentString.charAt(currentString.length() - 2) == '-' ) {
+//                        jumpIndex = parsedText.indexOf('\n');
+//                        currentString += parsedText.substring(jumpIndex);
+//                        parsedText = parsedText.substring(jumpIndex + 1);
+//                    }
+//                    System.out.println(currentString);
+//
+//                    if( currentString.contains("..") || currentString.contains("…") ) {
+//                        System.out.println(currentString);
+//                    }
+//
+//                }
+//
+
+                for( String thing : parsedText.split("\\n\\s+\\n") )
+                    System.out.println("!" + thing + "!");
+
+//
+//                int jumpIndexAuxiliar = 0;
+//                while ( parsedText.length() != 0 ) {
+//
+//                    jumpIndex =
+//                    //next thing
+//                    if( jumpIndex != -1 && jumpIndex < jumpIndexAuxiliar ) {
+//                        jumpIndex += 3;
+//                        currentString = parsedText.substring(0, jumpIndex - 3);
+//                    }
+//                    else
+//                        if( jumpIndexAuxiliar != -1 && jumpIndexAuxiliar < jumpIndex ) {
+//                            jumpIndex = jumpIndexAuxiliar;
+//                            jumpIndex += 4;
+//                            currentString = parsedText.substring(0, jumpIndex - 4);
+//
+//                        }
+//                    else
+//                        {
+//                            System.out.println("GATA");
+//                            break;
+//                        }
+//
+//                    System.out.println("!!!" + currentString + "!!!\n\n");
+//
+//                    if( !currentString.contains("..") || !currentString.contains("…") ) {
+//                        continue;
+//                    }
+//
+//                }
+
+                //System.out.println("!" + parsedText + "+");
+            }
+
+
+
+
+
+        } catch (IOException e) {
+            System.out.println("Eroare deschidere fisier");
+            System.exit(1);
+        }
+
+        /* Presupun ca mai toate pdf-uri de conferinte o sa aiba pe
+        *  pe prima pagina titlu, iar pe a doua detalii despre organizatori
+        *  sau alte lucruri. Asa ca o sa sar la pagina 3 direct, unde este cuprisnul
+        * */
+
     }
 }
--- src/converter/Publication.java
+++ src/converter/Publication.java
@@ -4,8 +4,8 @@ package converter;
  * Created by teodor on 18.02.2016.
  */
 public class Publication {
-    String author, email, name, topic;
-    int startPage, finishPage;
+    private String author, email, name, topic;
+    private int startPage, finishPage;
     //String content; (maybe)
 
     public Publication() {
@@ -29,6 +29,56 @@ public class Publication {
     public Publication( String name, String author, String topic, String email, int startPage, int finishPage ) {
         this(name, author, topic, startPage, finishPage);
         this.email = email;
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public int getStartPage() {
+        return startPage;
+    }
+
+    public void setStartPage(int startPage) {
+        this.startPage = startPage;
+    }
+
+    public int getFinishPage() {
+        return finishPage;
+    }
+
+    public void setFinishPage(int finishPage) {
+        this.finishPage = finishPage;
     }
 
     public String toString() {
